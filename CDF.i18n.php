84fbc11e465b3a19c1b5d520c809538498017738
Chris <chris@starforge.co.uk>
2012-04-24 20:30:22 +0100

    Added i18n support.

---
+++ CDF.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for Wolfram CDF extension
+ *
+ * @file
+ * @ingroup Extensions
+ * @license GNU General Public License 3.0 or later
+ */
+$messages = array();
+
+/** English
+ * @author Chris <chris@starforge.co.uk>
+ */
+$messages['en'] = array(
+        'wolfram_cdf_desc' => 'This extension provides the <nowiki><cdf></nowiki> tag',
+        'cdf_badfilename'  => 'Error in filename',
+);
